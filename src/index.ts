613f944acfb9d6b8e0eb13cc78cf1531893d6ef5
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-05 07:39:17 -0400

    created and imported port config file

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
-import express from "express"
-import path from "path"
+import express from "express";
+import path from "path";
 
-import index from "./routes/index"
-import form from "./routes/form"
+import { config } from "./config";
+import index from "./routes/index";
+import form from "./routes/form";
 
-const app: express.Express = express()
-const port = process.env.PORT || 3000
+const app: express.Express = express();
+const port = config.server.port;
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 //Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", index)
-app.use("/new", form)
+app.use("/", index);
+app.use("/new", form);
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
-})
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
