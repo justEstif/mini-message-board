19b395aef82f987902d470f7e61d802969682f3b
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-05 06:46:01 -0400

    basic post handle

--- src/routes/form.ts
+++ src/routes/form.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import { messages } from "./index"
 
 const router = express.Router()
 
@@ -6,8 +7,17 @@ router.get("/", (_, res, ___) => {
   res.render("form")
 })
 
-
-router.post("/", (_, res, ___) => {
-  res.render("form")
+router.post("/", (req, res, ___) => {
+  try {
+    messages.push({
+      message: req.body.message,
+      user: req.body.author,
+      sent: new Date(),
+    })
+    res.redirect("/")
+  } catch (err) {
+    console.log(err)
+  }
 })
+
 export default router
