bc774e474c2a541feedbf31a3a6546d623a610b7
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-05 05:28:18 -0400

    added body parser middleware and form router

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import path from "path"
 
 import index from "./routes/index"
+import form from "./routes/form"
 
 const app: express.Express = express()
 const port = process.env.PORT || 3000
@@ -10,7 +11,12 @@ const port = process.env.PORT || 3000
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
 
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/", index)
+app.use("/new", form)
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
